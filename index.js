c0cb2df45c91e9031adfa339c902b5d804b3af4b
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-06 14:33:50 -0600

    post request is now adding the info into the array. I need to figure out how to get the errors to work. I am pretty sure I know one of the two

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express');
+const shortid = require('shortid');
 
 const server = express();
 
 server.use(express.json());
 
+
+
+let users = [
+    {
+      id: "example",
+      name: "Test User",
+      bio: "Not Real - this is a test user."
+    }
+]
+
+
 server.get('/', (req, res) => {
     res.json({api: "running!!!!!"})
 })
 
+server.post(`/api/users`, (req, res) => {
+    const userInfo = req.body;
+    userInfo.id = shortid.generate();
+    users.push(userInfo);
+    res.status(201).json(userInfo)
+})
 
+server.get(`/api/users`, (req, res) => {
+    if(users){
+        res.status(200).json(users)
+    }else{
+        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+    }
+})
 
 
 
