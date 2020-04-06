ca05c21f895cbd62db95265d6b715d528cbeebc2
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-06 13:47:26 -0600

    added in express and server info for index.js. We'll be using port 5001

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("\n Yes it is \n")
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+
+const port = 5001;
+
+server.listen(port, ()=> console.log(`\n == api on port ${port} == \n`))
