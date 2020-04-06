e57b728e775afddc349c112ec837dd811c2e1476
Michael Callahan <CallahanMichaelE@gmail.com>
2020-04-06 16:41:44 -0600

    get request for users created. Need to set up the 500 errors with the try/catch

--- index.js
+++ index.js
@@ -36,18 +36,26 @@ server.post(`/api/users`, (req, res) => {
     }else{
         res.status(500).json({errorMessage: "There was an error while saving the user to the database"})
     }
-
 })
 
 server.get(`/api/users`, (req, res) => {
     if(users){
         res.status(200).json(users)
     }else{
-        res.status(500).json({ errorMessage: "The users information could not be retrieved." })
+        res.status(500).json({ errorMessage: "The users information could not be retrieved."})
     }
 })
 
+server.get(`/api/users/:id`, (req, res) => {
+    const id = req.params.id;
+    const user = users.find((use)=> use.id == id)
 
+    if(user){
+        res.status(200).json(user)
+    }else{
+        res.status(404).json({message: "The user with the specified ID does not exist."})
+    }
+})
 
 
 const port = 5001;
